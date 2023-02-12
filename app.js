cb9c813cf9ade2a4b6f915b91bae329247bb5ec4
edinmavric <edinmavric10@gmail.com>
2023-02-12 16:29:22 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = () => {
+    const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}?`, '').toUpperCase();
+    if (
+        selection !== ROCK && 
+        selection !== PAPER && 
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
+
+startGameBtn.addEventListener('click', function() {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
